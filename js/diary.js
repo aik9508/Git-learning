36f7fdc934623602b40f4f24c76410cd27b00f4f
Ke <Ke.Douglas.Wang@hotmail.com>
2018-04-28 19:52:36 +0200

    complete year mode

--- js/diary.js
+++ js/diary.js
@@ -31,6 +31,7 @@ function calendar_cell(cal_block,date,iscurrentmonth){
 calendar_cell.prototype.addDiaryListener = function(){
     const self = this;
     self.cell.off();
+    self.cell_diary.off();
     var mm = self.date.getMonth();
     var yy = self.date.getFullYear();
     var dd = self.date.getDate();
@@ -59,7 +60,8 @@ calendar_cell.prototype.addDiaryListener = function(){
             }
         });
     };
-    if(self.cal_block.mode==="mini"){
+    if(self.cal_block.mode!=="month"){
+        console.log('ok');
         self.cell.click(opendiary);
     }else{
         self.cell_diary.click(opendiary);
@@ -75,9 +77,7 @@ function calendar_block(cal, date){
     self.diarydates=[];
     self.block_wrapper = 
             $("<div class=calendar-block>"+
-                "<div class='month'>"+
-                    "<ul></ul>"+
-                "</div>"+
+                "<div class='block-title'></div>"+
                 "<ul class='weekdays'></ul>"+
                 "<div class='month-block-mask'>"+
                     "<div class='month-block'></div>"+
@@ -88,15 +88,9 @@ function calendar_block(cal, date){
     }else{
         self.mode="month";
     }
-    self.btn_next = $('<li class="next">&#10095;</li>');
-    self.btn_prev = $('<li class="prev">&#10094;</li>');
-    self.title = $('<li></li>'); // year and month
+    self.block_title = $(self.block_wrapper.find(".block-title")[0]);
     var month_block_mask = $(self.block_wrapper.find(".month-block-mask")[0]);
     self.month_block = $(month_block_mask.find("div")[0]);
-    $(self.block_wrapper.find(".month>ul")[0])
-                           .append(self.btn_prev)
-                           .append(self.btn_next)
-                           .append(self.title);
     for(var i=0; i<7; i++){
         $(self.block_wrapper.find(".weekdays")[0])
                                 .append($("<li>"
@@ -108,116 +102,14 @@ function calendar_block(cal, date){
 
 calendar_block.prototype.init = function(){
     const self=this;
-    self.title.html((self.mode==="year"?"":(self.currentdate.getFullYear()+"年"))
-                    +calendar.str_months[self.currentdate.getMonth()]);
-    self.fillcalendar();
-    if(self.mode === "year"){
-        return;
-    }
-    self.btn_next.click(function(){
-        var clines = self.month_block.find("div.calendar-line");
-        if(clines.length===6){
-            self.month_block.addClass("move-on");
-            var nnewclines = (self.end_date.getDate()<=7)?5:4;
-            self.start_date = new Date(self.end_date.getFullYear(),
-                                       self.end_date.getMonth(),1);
-            self.currentdate = new Date(self.start_date.getTime());
-            // the index of the first day of this month in self.cells
-            self.firstday = self.start_date.getDay(); 
-            self.start_date.setDate(self.start_date.getDate()-self.firstday);
-            var cdate = new Date(self.start_date.getTime());
-            cdate.setDate(cdate.getDate()-1+(6-nnewclines)*7);
-            for(var i=6-nnewclines;i<6;i++){
-                var calendar_line = $("<div class='calendar-line'></div>");
-                    for (var j=0;j<7;j++){
-                        cdate.setDate(cdate.getDate()+1);
-                        var ccell = new calendar_cell(self,cdate);
-                        self.cells.push(ccell);
-                        calendar_line.append(ccell.cell);
-                }
-                self.month_block.append(calendar_line);
-            }
-            self.end_date=new Date(cdate.getTime());
-            self.month_block.css(
-                    {"transform":"translate(0,-"
-                     +nnewclines*self.month_block.parent('div').height()/6
-                     +"px)"});
-            window.setTimeout(function(){
-                self.month_block.removeClass("move-on");
-                self.month_block.css({"transform":"translate(0,0)"});
-                self.month_block.find("div.calendar-line:lt("+nnewclines+")").remove();
-                self.cells.splice(0,nnewclines*7);
-                self.title.html(self.currentdate.getFullYear()+"年"+
-                               calendar.str_months[self.currentdate.getMonth()]);
-                $(self.cells).each(function(){
-                    if($(this)[0].date.getMonth()===self.currentdate.getMonth()){
-                        $(this)[0].cell_date_container.addClass("emphasize-date");
-                    }else{
-                        $(this)[0].cell_date_container.removeClass("emphasize-date");
-                    }
-                });
-                self.getDiaryDates();
-            },1000);
-        }
-    });
-    
-    self.btn_prev.click(function(){
-        var clines = self.month_block.find("div.calendar-line");
-        if(clines.length===6){
-            var tmp=new Date(self.start_date.getTime());
-            tmp.setDate(tmp.getDate()-1);
-            var tmp_cells=[];
-            var nnewclines=(tmp.getDate()>28)?5:4;
-            self.start_date = new Date(tmp.getFullYear(),tmp.getMonth(),1);
-            self.currentdate= new Date(self.start_date.getTime());
-            // the index of the first day of this month in self.cells
-            self.firstday = self.start_date.getDay(); 
-            self.start_date.setDate(self.start_date.getDate()-self.firstday);
-            var cdate = new Date(self.start_date.getTime());
-            cdate.setDate(cdate.getDate()-1);
-            for(var i=0;i<nnewclines;i++){
-                var calendar_line = $("<div class='calendar-line'></div>");
-                    for (var j=0;j<7;j++){
-                        cdate.setDate(cdate.getDate()+1);
-                        var ccell = new calendar_cell(self,cdate);
-                        tmp_cells.push(ccell);
-                        calendar_line.append(ccell.cell);
-                }
-                calendar_line.insertBefore(clines[0]);
-                self.month_block.css(
-                    {"transform":"translate(0,-"
-                     +(i+1)*self.month_block.parent('div').height()/6
-                     +"px)"});
-            }
-            self.end_date=
-                    new Date(self.cells[(6-nnewclines)*7-1].date.getTime());
-            window.setTimeout(function(){
-                    self.month_block.addClass("move-on");
-                    self.month_block.css({"transform":"translate(0,0)"});}
-            ,100);
-            window.setTimeout(function(){
-                self.month_block.removeClass("move-on");
-                self.month_block.find("div.calendar-line:gt(5)").remove();
-                self.cells.splice((6-nnewclines)*7,nnewclines*7);
-                self.cells=tmp_cells.concat(self.cells);
-                self.title.html(self.currentdate.getFullYear()+"年"+
-                               calendar.str_months[self.currentdate.getMonth()]);
-                $(self.cells).each(function(){
-                    if($(this)[0].date.getMonth()===self.currentdate.getMonth()){
-                        $(this)[0].cell_date_container.addClass("emphasize-date");
-                    }else{
-                        $(this)[0].cell_date_container.removeClass("emphasize-date");
-                    }
-                });
-                self.getDiaryDates();
-            },1100);
-        }
-    });
-                 
+    self.block_title.html(self.mode==="year"?
+                          calendar.str_months[self.currentdate.getMonth()]:
+                          "");
+    self.fillblock();            
 };
 
 // fill the calendar from a given date (6 lines)
-calendar_block.prototype.fillcalendar = function(){
+calendar_block.prototype.fillblock = function(){
     const self=this;
     var date = self.currentdate;
     self.start_date = new Date(date.getFullYear(),date.getMonth(),1);
@@ -226,6 +118,8 @@ calendar_block.prototype.fillcalendar = function(){
     self.start_date.setDate(self.start_date.getDate()-self.firstday);
     var cdate = new Date(self.start_date.getTime());
     cdate.setDate(cdate.getDate()-1);
+    self.month_block.html("");
+    self.cells = [];
     for(var i=0;i<6;i++){
         var calendar_line = $("<div class='calendar-line'></div>");
         for (var j=0;j<7;j++){
@@ -241,6 +135,117 @@ calendar_block.prototype.fillcalendar = function(){
     }
     self.end_date = new Date(cdate.getTime());
     self.getDiaryDates();
+};
+
+calendar_block.prototype.goToNextMonth = function(){
+    const self = this;
+    var clines = self.month_block.find("div.calendar-line");
+    if(clines.length===6){
+        self.month_block.addClass("move-on");
+        var nnewclines = (self.end_date.getDate()<=7)?5:4;
+        self.start_date = new Date(self.end_date.getFullYear(),
+                                   self.end_date.getMonth(),1);
+        self.currentdate = new Date(self.start_date.getTime());
+        // the index of the first day of this month in self.cells
+        self.firstday = self.start_date.getDay(); 
+        self.start_date.setDate(self.start_date.getDate()-self.firstday);
+        var cdate = new Date(self.start_date.getTime());
+        cdate.setDate(cdate.getDate()-1+(6-nnewclines)*7);
+        for(var i=6-nnewclines;i<6;i++){
+            var calendar_line = $("<div class='calendar-line'></div>");
+                for (var j=0;j<7;j++){
+                    cdate.setDate(cdate.getDate()+1);
+                    var ccell = new calendar_cell(self,cdate);
+                    self.cells.push(ccell);
+                    calendar_line.append(ccell.cell);
+            }
+            self.month_block.append(calendar_line);
+        }
+        self.end_date=new Date(cdate.getTime());
+        self.month_block.css(
+                {"transform":"translate(0,-"
+                 +nnewclines*self.month_block.parent('div').height()/6
+                 +"px)"});
+        window.setTimeout(function(){
+            self.month_block.removeClass("move-on");
+            self.month_block.css({"transform":"translate(0,0)"});
+            self.month_block.find("div.calendar-line:lt("+nnewclines+")").remove();
+            self.cells.splice(0,nnewclines*7);
+            $(self.cells).each(function(){
+                if($(this)[0].date.getMonth()===self.currentdate.getMonth()){
+                    $(this)[0].cell_date_container.addClass("emphasize-date");
+                }else{
+                    $(this)[0].cell_date_container.removeClass("emphasize-date");
+                }
+            });
+            self.getDiaryDates();
+        },1000);
+    }
+};
+
+calendar_block.prototype.goToPreviousMonth = function(){
+    const self = this;
+    var clines = self.month_block.find("div.calendar-line");
+    if(clines.length===6){
+        var tmp=new Date(self.start_date.getTime());
+        tmp.setDate(tmp.getDate()-1);
+        var tmp_cells=[];
+        var nnewclines=(tmp.getDate()>28)?5:4;
+        self.start_date = new Date(tmp.getFullYear(),tmp.getMonth(),1);
+        self.currentdate= new Date(self.start_date.getTime());
+        // the index of the first day of this month in self.cells
+        self.firstday = self.start_date.getDay(); 
+        self.start_date.setDate(self.start_date.getDate()-self.firstday);
+        var cdate = new Date(self.start_date.getTime());
+        cdate.setDate(cdate.getDate()-1);
+        for(var i=0;i<nnewclines;i++){
+            var calendar_line = $("<div class='calendar-line'></div>");
+                for (var j=0;j<7;j++){
+                    cdate.setDate(cdate.getDate()+1);
+                    var ccell = new calendar_cell(self,cdate);
+                    tmp_cells.push(ccell);
+                    calendar_line.append(ccell.cell);
+            }
+            calendar_line.insertBefore(clines[0]);
+            self.month_block.css(
+                {"transform":"translate(0,-"
+                 +(i+1)*self.month_block.parent('div').height()/6
+                 +"px)"});
+        }
+        self.end_date=
+                new Date(self.cells[(6-nnewclines)*7-1].date.getTime());
+        window.setTimeout(function(){
+                self.month_block.addClass("move-on");
+                self.month_block.css({"transform":"translate(0,0)"});}
+        ,100);
+        window.setTimeout(function(){
+            self.month_block.removeClass("move-on");
+            self.month_block.find("div.calendar-line:gt(5)").remove();
+            self.cells.splice((6-nnewclines)*7,nnewclines*7);
+            self.cells=tmp_cells.concat(self.cells);
+            $(self.cells).each(function(){
+                if($(this)[0].date.getMonth()===self.currentdate.getMonth()){
+                    $(this)[0].cell_date_container.addClass("emphasize-date");
+                }else{
+                    $(this)[0].cell_date_container.removeClass("emphasize-date");
+                }
+            });
+            self.getDiaryDates();
+        },1100);
+    }
+};
+
+calendar_block.prototype.goToNextYear = function(){
+    console.log('goToNextYear');
+    const self = this;
+    self.currentdate.setFullYear(self.currentdate.getFullYear()+1);
+    self.fillblock();
+};
+
+calendar_block.prototype.goToPreviousYear = function(){
+    const self = this;
+    self.currentdate.setFullYear(self.currentdate.getFullYear()-1);
+    self.fillblock();
 };
 
 // decides if it exists a diary written at a specific date.
@@ -280,16 +285,6 @@ calendar_block.prototype.addDiaryListeners = function(){
     });
 };
 
-calendar_block.prototype.setMode = function(mode){
-    const self = this;
-    self.mode = mode;
-    $(self.block_wrapper.find(".weekdays>li")).each(function(i){
-        $(this).html((self.mode === "month" ? "周" : "") +
-                     calendar.str_days[i]);
-    });
-    self.addDiaryListeners();
-};
-
 function calendar(mode){
     const self = this;
     self.mode = (mode)?mode:"month";
@@ -303,6 +298,9 @@ function calendar(mode){
                         "<li class='mode-day'>日</li>"+
                     "</ul>"+
                 "</div>"+
+                "<div class='title'>"+
+                    "<ul></ul>"+
+                "</div>"+
                 "<div class='calendar-blocks'></div>"+
               "</div>");
     self.menu = $(self.calendar_wrapper.find(".calendar-menu")[0]);
@@ -314,12 +312,26 @@ function calendar(mode){
     }else if(self.mode === "year"){
         self.btn_year.addClass("active");
     }
-    self.btn_year.click(function(){
-        self.setMode("year");
+    self.btn_month.click(function(){
+        if(self.mode!=="month"){
+            self.setMode("month");
+        }
     });
+    self.btn_year.click(function(){
+        if(self.mode!=="year"){
+            self.setMode("year");
+        }
+    });
+    self.btn_next = $('<li class="next">&#10095;</li>');
+    self.btn_prev = $('<li class="prev">&#10094;</li>');
+    self.title = $('<li></li>'); // year and month
+    $(self.calendar_wrapper.find(".title>ul")[0])
+                           .append(self.btn_prev)
+                           .append(self.btn_next)
+                           .append(self.title);
+    
     self.blocks_wrapper= $(self.calendar_wrapper.find(".calendar-blocks")[0]);
-    self.blocks = $([new calendar_block(self,self.currentdate)]);
-    self.blocks_wrapper.append(self.blocks[0].block_wrapper);
+    self.setMode(self.mode);
 }
 
 Object.defineProperty(calendar,"str_months",{
@@ -345,24 +357,24 @@ Object.defineProperty(calendar,"str_days",{
     configurable: true
 });
 
-calendar.prototype.init = function(){
-    const self = this;
-    self.blocks.each(function(){
-            this.init();
-    });
-};
-
 calendar.prototype.setMode = function(mode){
     const self = this;
     self.mode = mode;
+    self.title.html(self.currentdate.getFullYear()+"年"
+                    + (self.mode==="year" ? "" :
+                    calendar.str_months[self.currentdate.getMonth()]));
     self.calendar_wrapper.removeClass();
     self.calendar_wrapper.addClass(mode);
     self.calendar_wrapper.addClass("calendar");
+    self.blocks = $([]);
+    self.blocks_wrapper.html("");
+    $(self.calendar_wrapper.find(".calendar-menu li")).each(function(){
+        $(this).removeClass("active");
+    });
     if(mode === "year"){
         self.calendar_wrapper.addClass("mini");
+        self.btn_year.addClass("active");
         var yy = self.currentdate.getFullYear();
-        self.blocks = $([]);
-        self.blocks_wrapper.html("");
         for(var i=0; i<12; i++){
             var date = new Date(yy,i,1);
             var cblock = new calendar_block(self,date);
@@ -371,16 +383,65 @@ calendar.prototype.setMode = function(mode){
             self.blocks_wrapper.append(cblock.block_wrapper);
         }
     }else{
-        self.blocks.each(function(){
-            this.setMode(mode);
-        });
+        if(mode === "month"){
+            self.btn_month.addClass("active");
+        }else{
+            self.btn_day.addClass("active");
+        }
+        var cblock = new calendar_block(self,self.currentdate);
+        cblock.init();
+        self.blocks.push(cblock);
+        self.blocks_wrapper.append(cblock.block_wrapper);
     }
+    
+    self.btn_next.off();
+    self.btn_prev.off();
+    if(self.mode !== "year"){
+        self.btn_next.click(self.goToNextMonth.bind(self));
+        self.btn_prev.click(self.goToPreviousMonth.bind(self));
+    }else{
+        self.btn_next.click(self.goToNextYear.bind(self));
+        self.btn_prev.click(self.goToPreviousYear.bind(self));
+    }
+};
+
+calendar.prototype.goToNextMonth = function(){
+    const self = this;
+    self.blocks[0].goToNextMonth.bind(self.blocks[0])();
+    self.currentdate.setMonth(self.currentdate.getMonth()+1);
+    self.title.html(self.currentdate.getFullYear()+"年"+
+                           calendar.str_months[self.currentdate.getMonth()]);
+};
+
+calendar.prototype.goToPreviousMonth = function(){
+    const self = this;
+    self.blocks[0].goToPreviousMonth.bind(self.blocks[0])();
+    self.currentdate.setMonth(self.currentdate.getMonth()-1);
+    self.title.html(self.currentdate.getFullYear()+"年"+
+                           calendar.str_months[self.currentdate.getMonth()]);
+};
+
+calendar.prototype.goToNextYear = function(){
+    const self = this;
+    self.blocks.each(function(){
+        this.goToNextYear();
+    });
+    self.currentdate.setFullYear(self.currentdate.getFullYear()+1);
+    self.title.html(self.currentdate.getFullYear()+"年");
+};
+
+calendar.prototype.goToPreviousYear = function(){
+    const self = this;
+    self.blocks.each(function(){
+        this.goToPreviousYear();
+    });
+    self.currentdate.setFullYear(self.currentdate.getFullYear()-1);
+    self.title.html(self.currentdate.getFullYear()+"年");
 };
 
 $(document).ready(function(){
     var mycalendar = new calendar();
     $("#mycalendar").append(mycalendar.calendar_wrapper);
-    mycalendar.init();
 });
 
 
