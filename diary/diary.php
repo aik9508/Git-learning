59112b0ce502d62b128d7e9018aee3a910f84d95
Ke <Ke.Douglas.Wang@hotmail.com>
2018-05-01 08:42:48 +0200

    set background

--- diary/diary.php
+++ diary/diary.php
@@ -18,37 +18,36 @@
         <script src="../js/diary.js"></script>
     </head>
     <body>
-        <h1>Diary</h1>
         <div class="container">
             <div class="row">
+                <h1>Diary</h1>
                 <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <a class="navbar-brand" href="#">Aik</a>
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <a class="navbar-brand" href="#">Aik</a>
+                            </div>
+                            <ul class="nav navbar-nav">
+                                <li><a href="../index.php">Home</a></li>
+                                <li class="active"><a href="#">Diary</a></li>
+                                <li><a href="#">History</a></li>
+                                <li><a href="#">Photos</a></li>
+                            </ul>
                         </div>
-                        <ul class="nav navbar-nav">
-                            <li><a href="../index.php">Home</a></li>
-                            <li class="active"><a href="#">Diary</a></li>
-                            <li><a href="#">History</a></li>
-                            <li><a href="#">Photos</a></li>
-                        </ul>
-                    </div>
-                </nav>
-                <div class="big-wrapper">
+                    </nav>
+            </div>
+        </div>
+        <div class="big-wrapper">           
+            <div class="container wrapper">
+                <div class="row">
                     <div id="mycalendar">
 
                     </div>
                 </div>
             </div>
         </div>
-        
         <footer>
             <div>Last updated : April 21, 2018</div>
             <div>Author : Ke Wang</div>
         </footer>
-    <script>
-        var str_months = ["january","feburary","march","april","may","june","july","august",
-        "september","october","november","december"];
-    </script>
     </body>
 </html>
